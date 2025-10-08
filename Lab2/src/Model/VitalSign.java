f8927f681813cdd0551b5c7fd8fc4c6715f05296
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-08 01:04:51 -0400

    lab2
    Managing Multiple Objects

---
+++ Lab2/src/Model/VitalSign.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author rithwikvarma
+ */
+public class VitalSign {
+    String date;
+    float temperature;
+    double bloodPressure;
+    int pulse;
+    boolean isConscious;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(double bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+    public boolean isIsConscious() {
+        return isConscious;
+    }
+
+    public void setIsConscious(boolean isConscious) {
+        this.isConscious = isConscious;
+    }
+  
+    @Override
+    public String toString(){
+        return getDate();
+    }
+    
+}
