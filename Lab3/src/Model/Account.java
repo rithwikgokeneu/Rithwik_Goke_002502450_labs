9be6c2658d8c38dfe9a5a9d6ac47b3571990628e
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-09 01:21:42 -0400

    Add Account and AccountDirectory model classes
    Add Account and AccountDirectory model classes

--- Lab3/src/Model/Account.java
+++ Lab3/src/Model/Account.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Add Account and AccountDirectory model classes
  */
 package Model;
 
