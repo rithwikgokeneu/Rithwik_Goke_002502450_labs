ac0d6d112623ce70602c7588927b52acbcb45709
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-09 18:05:47 -0400

    Introduction to Java Abstract, User Login procedure and work area
    Introduction to Java Abstract, User Login procedure and work area

---
+++ Lab5/src/Business/Business.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Business.Person.PersonDirectory;
+import Business.Profiles.EmployeeDirectory;
+import Business.Profiles.FacultyDirectory;
+import Business.Profiles.StudentDirectory;
+
+import Business.UserAccounts.UserAccountDirectory;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class Business {
+
+    String name;
+    PersonDirectory persondirectory; //all people profiles regardless of the role
+
+    EmployeeDirectory employeedirectory;
+    UserAccountDirectory useraccountdirectory;
+    StudentDirectory studentdirectory;
+    FacultyDirectory facultydirectory;
+    
+
+
+    public Business(String n) {
+        name = n;
+
+        persondirectory = new PersonDirectory();
+        employeedirectory = new EmployeeDirectory(this);
+        useraccountdirectory = new UserAccountDirectory();
+        studentdirectory = new StudentDirectory();
+        facultydirectory = new FacultyDirectory();
+
+
+    }
+
+    public PersonDirectory getPersonDirectory() {
+        return persondirectory;
+    }
+
+    public UserAccountDirectory getUserAccountDirectory() {
+        return useraccountdirectory;
+    }
+
+
+    public EmployeeDirectory getEmployeeDirectory() {
+        return employeedirectory;
+    }
+
+    public StudentDirectory getStudentDirectory(){
+        return studentdirectory;
+    }
+        
+     public FacultyDirectory getFacultyDirectory() {
+        return facultydirectory;
+    }
+
+}
---
+++ Lab5/src/Business/Profiles/FacultyProfile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Profiles;
+
+import Business.Person.Person;
+
+/**
+ *
+ * @author rithwikvarma
+ */
+public class FacultyProfile extends Profile  {
+    public FacultyProfile(Person p) {
+        super(p); // pass the Person to Profile
+    }
+
+    @Override
+    public String getRole() {
+        return "Faculty";
+    }
+}
