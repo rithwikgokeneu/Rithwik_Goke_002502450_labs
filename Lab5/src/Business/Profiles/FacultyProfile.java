6bd329c905d79892b61f623f52d04753cf63c914
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-09 18:09:46 -0400

    Add core business models, profiles, and user account management
    Add core business models, profiles, and user account management

--- Lab5/src/Business/Profiles/FacultyProfile.java
+++ Lab5/src/Business/Profiles/FacultyProfile.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Add core business models, profiles, and user account management
  */
 package Business.Profiles;
 
