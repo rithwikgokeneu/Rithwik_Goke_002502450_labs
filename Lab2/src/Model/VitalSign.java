f66336e2d7f92695d28ce2dd9afba39dd9337b6f
rithwikgokeneu <goke.r@northeastern.edu>
2025-10-09 01:02:37 -0400

    Add VitalSign and VitalSignHistory model classes for storing and tracking patient vitals
    Add VitalSign and VitalSignHistory model classes for storing and tracking patient vitals

--- Lab2/src/Model/VitalSign.java
+++ Lab2/src/Model/VitalSign.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Add VitalSign and VitalSignHistory model classes for storing and tracking patient vitals
  */
 package Model;
 
