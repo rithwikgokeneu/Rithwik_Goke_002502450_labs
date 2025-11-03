f9bc1f33963a31e38b6c985ea57e01a074a6f061
rithwikgokeneu <goke.r@northeastern.edu>
2025-11-02 21:05:44 -0700

    Order Processing
    Order Processing

---
+++ Lab7/src/model/Business.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rithwikvarma
+ */
+public class Business {
+    
+    MasterOrderList masterOderList;
+    SupplierDirectory supplierDirectory;
+
+    public Business() {
+        
+        masterOderList = new MasterOrderList();   
+        supplierDirectory = new SupplierDirectory();
+    
+    }
+
+    public MasterOrderList getMasterOderList() {
+        return masterOderList;
+    }
+
+    public void setMasterOderList(MasterOrderList masterOderList) {
+        this.masterOderList = masterOderList;
+    }
+
+    public SupplierDirectory getSupplierDirectory() {
+        return supplierDirectory;
+    }
+
+    public void setSupplierDirectory(SupplierDirectory supplierDirectory) {
+        this.supplierDirectory = supplierDirectory;
+    }
+    
+    
+    
+}
