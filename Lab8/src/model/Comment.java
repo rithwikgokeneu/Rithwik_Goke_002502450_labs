8e716795278f4ea2cb4b207b3a6a0fc4378f24fe
rithwikgokeneu <goke.r@northeastern.edu>
2025-11-09 02:19:32 -0500

    Initialize project structure and add model classes (User, Post, Comment)
    Initialize project structure and add model classes (User, Post, Comment)

--- Lab8/src/model/Comment.java
+++ Lab8/src/model/Comment.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Initialize project structure and add model classes (User, Post, Comment)
  */
 package model;
 
